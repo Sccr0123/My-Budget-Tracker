412aab734c0a7a73eca7bf473ef63445dbeeecb6
Zachary Thomas <sccr0123@gmail.com>
2022-06-09 22:57:54 -0400

    Enabled IndexedDB

---
+++ public/js/idb.js
@@ -0,0 +1,77 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+	const db = event.target.result;
+
+	db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+	db = event.target.result;
+
+	if (navigator.onLine) {
+		sendTransaction();
+	}
+};
+
+request.onerror = function (event) {
+	console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+	const transaction = db.transaction(["new_transaction"], "readwrite");
+
+	const transactionObjectStore = transaction.objectStore("new_transaction");
+
+	// add record to your store with add method
+	transactionObjectStore.add(record);
+}
+
+function sendTransaction() {
+	// open a transaction on your pending db
+	const transaction = db.transaction(["new_transaction"], "readwrite");
+
+	// access your pending object store
+	const transactionObjectStore = transaction.objectStore("new_transaction");
+
+	// get all records from store and set to a variable
+	const getAll = transactionObjectStore.getAll();
+
+	getAll.onsuccess = function () {
+		// if there was data in indexedDb's store, let's send it to the api server
+		if (getAll.result.length > 0) {
+			fetch("/api/transaction", {
+				method: "POST",
+				body: JSON.stringify(getAll.result),
+				headers: {
+					Accept: "application/json, text/plain, */*",
+					"Content-Type": "application/json",
+				},
+			})
+				.then((response) => response.json())
+				.then((serverResponse) => {
+					if (serverResponse.message) {
+						throw new Error(serverResponse);
+					}
+
+					const transaction = db.transaction(
+						["new_transaction"],
+						"readwrite"
+					);
+					const transactionObjectStore =
+						transaction.objectStore("new_transaction");
+					// clear all items in your store
+					transactionObjectStore.clear();
+				})
+				.catch((err) => {
+					// set reference to redirect back here
+					console.log(err);
+				});
+		}
+	};
+}
+
+// listen for app coming back online
+window.addEventListener("online", sendTransaction);
