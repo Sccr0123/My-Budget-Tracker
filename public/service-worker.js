e4bceb4fb0697623798717c366a5b8b088bd3b59
Zachary Thomas <sccr0123@gmail.com>
2022-06-14 18:59:34 -0400

    Fixed manifest.json  bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,6 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const DATA_CACHE_NAME = "data-cache-" + VERSION;
 const FILES_TO_CACHE = [
-	"/",
 	"./index.html",
 	"./css/styles.css",
 	"./js/idb.js",
@@ -20,65 +19,65 @@ const FILES_TO_CACHE = [
 ];
 
 //Respond with cached resources
-self.addEventListener("fetch", function (e) {
-	console.log("fetch request : " + e.request.url);
-	e.respondWith(
-		caches.match(e.request).then(function (request) {
-			// if (request) {
-			// 	// if cache is available, respond with cache
-			// 	console.log("responding with cache : " + e.request.url);
-			// 	return request;
-			// } else {
-			// 	// if there are no cache, try fetching request
-			// 	console.log("file is not cached, fetching : " + e.request.url);
-			// 	return fetch(e.request);
-			// }
+// self.addEventListener("fetch", function (e) {
+// 	console.log("fetch request : " + e.request.url);
+// 	e.respondWith(
+// 		caches.match(e.request).then(function (request) {
+// 			// if (request) {
+// 			// 	// if cache is available, respond with cache
+// 			// 	console.log("responding with cache : " + e.request.url);
+// 			// 	return request;
+// 			// } else {
+// 			// 	// if there are no cache, try fetching request
+// 			// 	console.log("file is not cached, fetching : " + e.request.url);
+// 			// 	return fetch(e.request);
+// 			// }
 
-			// You can omit if/else for console.log & put one line below like this too.
-			return request || fetch(e.request);
-		})
-	);
-});
-
-// self.addEventListener("fetch", function (event) {
-// 	// cache all get requests to /api routes
-// 	if (event.request.url.includes("/api/")) {
-// 		event.respondWith(
-// 			caches
-// 				.open(DATA_CACHE_NAME)
-// 				.then((cache) => {
-// 					return fetch(event.request)
-// 						.then((response) => {
-// 							// If the response was good, clone it and store it in the cache.
-// 							if (response.status === 200) {
-// 								cache.put(event.request.url, response.clone());
-// 							}
-// 							return response;
-// 						})
-// 						.catch((err) => {
-// 							// Network request failed, try to get it from the cache.
-// 							return cache.match(event.request);
-// 						});
-// 				})
-// 				.catch((err) => console.log(err))
-// 		);
-// 		return;
-// 	}
-// 	event.respondWith(
-// 		fetch(event.request).catch(function () {
-// 			return caches.match(event.request).then(function (response) {
-// 				if (response) {
-// 					return response;
-// 				} else if (
-// 					event.request.headers.get("accept").includes("text/html")
-// 				) {
-// 					// return the cached home page for all requests for html pages
-// 					return caches.match("/");
-// 				}
-// 			});
+// 			// You can omit if/else for console.log & put one line below like this too.
+// 			return request || fetch(e.request);
 // 		})
 // 	);
 // });
+
+self.addEventListener("fetch", function (event) {
+	// cache all get requests to /api routes
+	if (event.request.url.includes("/api/")) {
+		event.respondWith(
+			caches
+				.open(DATA_CACHE_NAME)
+				.then((cache) => {
+					return fetch(event.request)
+						.then((response) => {
+							// If the response was good, clone it and store it in the cache.
+							if (response.status === 200) {
+								cache.put(event.request.url, response.clone());
+							}
+							return response;
+						})
+						.catch((err) => {
+							// Network request failed, try to get it from the cache.
+							return cache.match(event.request);
+						});
+				})
+				.catch((err) => console.log(err))
+		);
+		return;
+	}
+	event.respondWith(
+		fetch(event.request).catch(function () {
+			return caches.match(event.request).then(function (response) {
+				if (response) {
+					return response;
+				} else if (
+					event.request.headers.get("accept").includes("text/html")
+				) {
+					// return the cached home page for all requests for html pages
+					return caches.match("/");
+				}
+			});
+		})
+	);
+});
 
 // Cache resources
 self.addEventListener("install", function (e) {
